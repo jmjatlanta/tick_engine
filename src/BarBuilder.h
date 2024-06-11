65bec3ee51e197b9591cac1c4726a54b8e2a7c6e
jmjatlanta <jmjatlanta@gmail.com>
2024-06-11 13:50:55 -0500

    beginnings of BarBuilder

--- src/BarBuilder.h
+++ src/BarBuilder.h
@@ -3,6 +3,8 @@
 #include <vector>
 #include <cstdint>
 #include <string_view>
+#include <string>
+#include <unordered_map>
 
 enum class BarDuration
 {
@@ -37,14 +39,29 @@ public:
     virtual void OnBar(const std::string_view contract, BarDuration duration, const Bar bar) = 0;
 };
 
+/***
+ * A simplistic bar builder. It listens for ticks, and builds bars, firing events when a bar
+ * is updated
+ */
 class BarBuilder
 {
     public:
     BarBuilder(const std::string_view contract);
 
-    void AddHandler(std::shared_ptr<BarHandler> in, BarDuration duration);
+    /***
+    * Add a new listener
+    * @param in the listener
+    */
+    void AddHandler(std::shared_ptr<BarHandler> in);
     void RemoveHandler(std::shared_ptr<BarHandler> in);
+    /****
+    * Start sending bars of a particular contract/duration
+    * @param contract the contract
+    * @param duration the duration
+    */
+    void AddDuration(const std::string_view contract, BarDuration duration);
 
     protected:
-    std::vector<std::shared_ptr<BarHandler>> handlers;
+    std::vector<std::shared_ptr<BarHandler>> handlers; // all receive all messages
+    std::unordered_map<std::string, std::vector<Bar>> latestBars; // contract to collection of bars, 1 per durration
 };
